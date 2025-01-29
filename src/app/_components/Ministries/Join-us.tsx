06ee3374c2fa3b3603ad2eab155aaf7480c0ca16
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2025-01-29 21:46:27 +0300

    fix:reponsiveness on ministry page

--- src/app/_components/Ministries/Join-us.tsx
+++ src/app/_components/Ministries/Join-us.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function JoinUs() {
   return (
-    <section className="max-w-screen-2xl mx-auto px-8 py-28 flex justify-between items-center">
+    <section className="max-w-screen-2xl mx-auto px-8 py-28 flex gap-8 flex-wrap justify-between items-center">
       <div className="flex flex-col gap-6">
         <h1>Join Our Vibrant Ministries Today</h1>
         <p className="text-lg">Discover how you can make a difference.</p>
